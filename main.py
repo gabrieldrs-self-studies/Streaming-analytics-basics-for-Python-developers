8ad337e6ad4cb5111c63d7caefc13cef3b1271fe
Gabriel R. Sezefredo <gabriel@sezefredo.com.br>
2018-07-22 15:23:09 -0300

    Collect data average
    This commit adds one more map, now using a callable class.
    The callable class is responsible for keeping track of the last n values received from the stream and calculating their average value.
    A helper function "getReadingValue" was imported so we can easily access the heart rate value of tuples we receive.
    
    Signed-off-by: Gabriel R. Sezefredo <gabriel@sezefredo.com.br>

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from streams.observations import observation_stream  # Cannot use "as X"
 
 import hashlib
 
+from streamsx_health.ingest.Observation import getReadingValue
+
 service_name = "Streaming Analytics-no"  # Service name
 credentials = {}  # Put redentials here
 
@@ -13,6 +15,20 @@ def anonymize(tuple_):
     tuple_['device']['locationId'] = hashlib.sha256(tuple_['device']['locationId'].encode('utf - 8')).digest()
 
     return tuple_
+
+class Avg(object):
+
+    def __init__(self, max_):
+        self.history_max = max_  # The maximum length of the array we will use to calculate the average
+        self.last_n = []
+
+    # Object is callable
+    def __call__(self, tuple_):
+        self.last_n.append(getReadingValue(tuple_))
+        if len(self.last_n) > self.history_max:
+            self.last_n.pop(0)
+
+        return sum(self.last_n) / len(self.last_n)
 
 
 # Set up access to Streaming Analytics service
@@ -34,8 +50,11 @@ anonymous_patient_stream = patient_data_stream.map(anonymize)
 # Filtering data
 heart_rate_stream = anonymous_patient_stream.filter(lambda tuple_: (tuple_['reading']['readingType']['code'] == '8867-4'))
 
+# Calculate the patient's heart rate average based on the latest 10 tuples
+heart_average_stream = heart_rate_stream.map(Avg(10))
+
 # Print data stream
-heart_rate_stream.sink(print)
+heart_average_stream.sink(print)
 
 # Submit on Bluemix
 submit(ContextTypes.ANALYTICS_SERVICE, topo, cfg)

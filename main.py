bee4308b3ac936c53f47431cf33e1e044a3c656f
Gabriel R. Sezefredo <gabriel@sezefredo.com.br>
2018-07-22 11:30:55 -0300

    Adding mapping
    This adds a mapping to the stream pipeline that anonymizes patients, hashing their id and location.
    This step is added to the pipeline before the filter, using the stream function map.
    
    Signed-off-by: Gabriel R. Sezefredo <gabriel@sezefredo.com.br>

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ from streamsx.topology import schema
 from streamsx.topology.context import *
 from streams.observations import observation_stream  # Cannot use "as X"
 
+import hashlib
+
 service_name = "Streaming Analytics-no"  # Service name
 credentials = {}  # Put redentials here
+
+def anonymize(tuple_):
+    tuple_['patientId'] = hashlib.sha256(tuple_['patientId'].encode('utf - 8')).digest()
+    tuple_['device']['locationId'] = hashlib.sha256(tuple_['device']['locationId'].encode('utf - 8')).digest()
+
+    return tuple_
+
 
 # Set up access to Streaming Analytics service
 vs = {'streaming-analytics': [{'name': service_name, 'credentials': credentials}]}
@@ -19,8 +28,11 @@ cfg = {
 topo = Topology()
 patient_data_stream = topo.subscribe('ingest-beacon', schema.CommonSchema.Json)
 
+# Anonymize patient
+anonymous_patient_stream = patient_data_stream.map(anonymize)
+
 # Filtering data
-heart_rate_stream = patient_data_stream.filter(lambda tuple_: (tuple_['reading']['readingType']['code'] == '8867-4'))
+heart_rate_stream = anonymous_patient_stream.filter(lambda tuple_: (tuple_['reading']['readingType']['code'] == '8867-4'))
 
 # Print data stream
 heart_rate_stream.sink(print)
